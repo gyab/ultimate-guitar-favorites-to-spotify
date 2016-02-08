362414ec0271148c8f99829251d39aecadebf742
Antoine Mary <antoinemary@outlook.com>
2016-02-08 08:16:43 +0100

    fixed last commit with the rights updated fles

--- content.js
+++ content.js
@@ -6,97 +6,149 @@ $(document).ready(function() {
     iDiv.innerHTML = "<button id='checkPage'>Export to Spotify</button><button class='btn btn-primary' id='btn-login'>Login</button><div id='result'></div><script id='result-template' type='text/x-handlebars-template'><dl><img src='{{images.0.url}}'><dt>User Name</dt><dd>{{id}}</dd><dt>Display Name</dt><dd>{{display_name}}</dd><dt>Country</dt><dd>{{country}}</dd><dt>Followers</dt><dd>{{followers.total}}</dd><dt>Profile</dt><dd><a href='{{external_urls.spotify}}' target='_blank'>{{external_urls.spotify}}</a></dd><dt>Email</dt><dd>{{email}}</dd><dt>Product</dt><dd>{{product}}</dd></dl></script>";
     iDiv.id = 'block';
     iDiv.className = 'block';
-    document.body.insertBefore( iDiv, document.body.firstChild );
+    document.body.insertBefore(iDiv, document.body.firstChild);
 
-    document.getElementById('checkPage').addEventListener("click", function () {
-        chrome.runtime.sendMessage({from: "content", data: getData()});
+    document.getElementById('checkPage').addEventListener("click", function() {
+        chrome.runtime.sendMessage({
+            from: "content",
+            data: getData()
+        });
     });
 
     var templateSource = document.getElementById('result-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    resultsPlaceholder = document.getElementById('result'),
-    loginButton = document.getElementById('btn-login');
+        template = Handlebars.compile(templateSource),
+        resultsPlaceholder = document.getElementById('result'),
+        loginButton = document.getElementById('btn-login');
 
     loginButton.addEventListener('click', function() {
         login(function(accessToken) {
-          getUserData(accessToken)
-              .then(function(response) {
-                loginButton.style.display = 'none';
-                createPlaylist(accessToken, response.id);
-              });                  
+            getUserData(accessToken)
+                .then(function(response) {
+                    var track_id;
+                    getTrack(accessToken, track_id)
+                        .then(function(response) {
+                            console.log(track_id);
+                            addToPlaylist(accessToken, "ahem", "0UBBuHt9ZNiqEgAxzbIh0I", track_id); 
+                        });
+                    //loginButton.style.display = 'none';
+                    //createPlaylist(accessToken, response.id);
+                });
         });
     });
+
+    function getTrack(accessToken, track_id) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.spotify.com/v1/search",
+            data: 
+                {
+                    "q": "Arctic Monkeys Riot Van",
+                    "type": "track"
+                }
+            ,
+            dataType: "json",
+            contentType: "application/json",
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            },
+            success: function(data){
+                track_id = data.tracks.items[0].artists[0].id;
+                console.log(track_id);
+            }
+        });     
+    }
+
+    function addToPlaylist(accessToken, user_id, playlist_id, track_id) {
+        return $.ajax({
+            type: "POST",
+            url: "https://api.spotify.com/v1/users/"+ user_id +"/playlists/"+ playlist_id +"/tracks",
+            data: {
+                "uris": "spotify:track:" + track_id,
+            },
+            dataType: "json",
+            contentType: "application/json",
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+            },
+            success: function(data){
+                console.log(data);
+            }
+        });
+    }
 
     function createPlaylist(accessToken, id) {
         return $.ajax({
             type: "POST",
-            url: "https://api.spotify.com/v1/users/"+id+"/playlists",
-            data : JSON.stringify({ "name":"A New Playlist", "public":"false" }),
+            url: "https://api.spotify.com/v1/users/" + id + "/playlists",
+            data: JSON.stringify({
+                "name": "A New Playlist",
+                "public": "false"
+            }),
             dataType: "json",
             contentType: "application/json",
             headers: {
-               'Authorization': 'Bearer ' + accessToken,
+                'Authorization': 'Bearer ' + accessToken,
             }
         });
     }
-    
+
     function login(callback) {
-        var CLIENT_ID = '43bf163455494a0baafa85e8ec2f7873';
+        var CLIENT_ID = "e78c4b5a89334dea898452bfe6b1c1e5";
         var REDIRECT_URI = 'http://antoinemary.com/ug-to-spotify';
         scopes = 'playlist-read-private playlist-modify-private';
+
         function getLoginURL(scopes) {
             return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
-              '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
-              '&scope=' + encodeURIComponent(scopes.join(' ')) +
-              '&response_type=token';
+                '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+                '&scope=' + encodeURIComponent(scopes.join(' ')) +
+                '&response_type=token';
         }
-        
+
         var url = getLoginURL([
             'playlist-read-private playlist-modify-private'
         ]);
-        
+
         var width = 450,
             height = 730,
             left = (screen.width / 2) - (width / 2),
             top = (screen.height / 2) - (height / 2);
-    
+
         window.addEventListener("message", function(event) {
             var hash = JSON.parse(event.data);
             if (hash.type == 'access_token') {
                 callback(hash.access_token);
             }
         }, false);
-        
+
         var w = window.open(url,
-                            'Spotify',
-                            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
-                           );
-        
+            'Spotify',
+            'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+        );
+
     }
 
     function getUserData(accessToken) {
         return $.ajax({
             url: 'https://api.spotify.com/v1/me',
             headers: {
-               'Authorization': 'Bearer ' + accessToken,
+                'Authorization': 'Bearer ' + accessToken,
 
             }
         });
     }
 
-    function getData()
-    {
+    function getData() {
         //get html element based on regex (code from http://james.padolsey.com/snippets/regex-selector-for-jquery/)
         jQuery.expr[':'].regex = function(elem, index, match) {
             var matchParams = match[3].split(','),
                 validLabels = /^(data|css):/,
                 attr = {
-                    method: matchParams[0].match(validLabels) ? 
-                                matchParams[0].split(':')[0] : 'attr',
-                    property: matchParams.shift().replace(validLabels,'')
+                    method: matchParams[0].match(validLabels) ?
+                        matchParams[0].split(':')[0] : 'attr',
+                    property: matchParams.shift().replace(validLabels, '')
                 },
                 regexFlags = 'ig',
-                regex = new RegExp(matchParams.join('').replace(/^s+|s+$/g,''), regexFlags);
+                regex = new RegExp(matchParams.join('').replace(/^s+|s+$/g, ''), regexFlags);
             return regex.test(jQuery(elem)[attr.method](attr.property));
         }
 
@@ -110,12 +162,13 @@ $(document).ready(function() {
         $('tr:regex(tabindex,-1)').each(function() {
             var artist = $(this).find('td:nth-child(3)').text() ? $(this).find('td:nth-child(3)').text() : songs[songs.length - 1][0]
             songs.push([
-              artist, 
-              $(this).find('td:nth-child(4)').text()
+                artist,
+                $(this).find('td:nth-child(4)').text()
                 .replace(" Acoustic", '')
                 .replace(" Chords", '')
                 .replace(" Tab", "")
-                .replace(/\s\(ver \d+\)/, "")]);
+                .replace(/\s\(ver \d+\)/, "")
+            ]);
         });
 
         return songs;
