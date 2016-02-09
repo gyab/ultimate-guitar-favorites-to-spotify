133ff202ac875bb499b92fb172069d0618eff24b
Antoine Mary <antoinemary@outlook.com>
2016-02-09 15:49:09 +0100

    clean up repository

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.from == "content") {
-      	//chrome.runtime.sendMessage({from: "background"});
-       	console.log(request.data[0]);
-	}
-  }
-);
