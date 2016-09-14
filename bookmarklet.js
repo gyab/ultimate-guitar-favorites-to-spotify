42746e049c15f13bf562b81e8adb94553eb5069e
Antoine Mary <antoinemary@outlook.com>
2016-09-14 16:42:10 +0200

    bookmarklet added

---
+++ bookmarklet.js
@@ -0,0 +1,168 @@
+	login(function(accessToken) {
+	    getUserData(accessToken)
+	        .then(function(response) {
+	            songs = getData();
+	            createPlaylist(accessToken, response.id);
+	        });
+	});
+
+	function login(callback) {
+	    var CLIENT_ID = "e78c4b5a89334dea898452bfe6b1c1e5";
+	    var REDIRECT_URI = 'http://antoinemary.com/ug-to-spotify';
+	    scopes = 'playlist-read-private playlist-modify-private';
+
+	    function getLoginURL(scopes) {
+	        return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+	            '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+	            '&scope=' + encodeURIComponent(scopes.join(' ')) +
+	            '&response_type=token';
+	    }
+
+	    var url = getLoginURL([
+	        'playlist-read-private playlist-modify-private'
+	    ]);
+
+	    var width = 450,
+	        height = 730,
+	        left = (screen.width / 2) - (width / 2),
+	        top = (screen.height / 2) - (height / 2);
+
+	    window.addEventListener("message", function(event) {
+	        var hash = JSON.parse(event.data);
+	        if (hash.type == 'access_token') {
+	            callback(hash.access_token);
+	        }
+	    }, false);
+
+	    var w = window.open(url,
+	        'Spotify',
+	        'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left
+	    );
+
+	}
+
+	var idUser = null;
+
+	function createPlaylist(accessToken, id) {
+	    var playlistName = window.prompt("Playlist name", "Type the name you want for the playlist");
+	    if (playlistName) {
+	        return $.ajax({
+	            type: "POST",
+	            url: "https://api.spotify.com/v1/users/" + id + "/playlists",
+	            data: JSON.stringify({
+	                "name": playlistName,
+	                "public": "false"
+	            }),
+	            dataType: "json",
+	            contentType: "application/json",
+	            headers: {
+	                'Authorization': 'Bearer ' + accessToken,
+	            },
+	            success: function(data) {
+	                getTrack(accessToken, data['id']);
+	            }
+	        });
+	    }
+	}
+
+
+	function addToPlaylist(accessToken, user_id, playlist_id, idTracks) {
+	    $.ajax({
+	        type: "POST",
+	        url: "https://api.spotify.com/v1/users/" + user_id + "/playlists/" + playlist_id + "/tracks/",
+	        data: idTracks,
+	        dataType: "json",
+	        contentType: "application/json",
+	        headers: {
+	            'Authorization': 'Bearer ' + accessToken
+	        },
+	        success: function(data) {}
+	    });
+	}
+
+
+	function getUserData(accessToken) {
+	    return $.ajax({
+	        url: 'https://api.spotify.com/v1/me',
+	        headers: {
+	            'Authorization': 'Bearer ' + accessToken,
+
+	        },
+	        success: function(data) {
+	            idUser = data.id;
+	        }
+	    });
+	}
+
+	function getData() {
+	    //get html element based on regex (code from http://james.padolsey.com/snippets/regex-selector-for-jquery/)
+	    jQuery.expr[':'].regex = function(elem, index, match) {
+	        var matchParams = match[3].split(','),
+	            validLabels = /^(data|css):/,
+	            attr = {
+	                method: matchParams[0].match(validLabels) ?
+	                    matchParams[0].split(':')[0] : 'attr',
+	                property: matchParams.shift().replace(validLabels, '')
+	            },
+	            regexFlags = 'ig',
+	            regex = new RegExp(matchParams.join('').replace(/^s+|s+$/g, ''), regexFlags);
+	        return regex.test(jQuery(elem)[attr.method](attr.property));
+	    }
+
+	    //declare array
+	    var songs = [];
+
+	    //simulate click to get all the songs
+	    $("#pager_left").find("a")[1].click();
+
+	    //For each td, get the artist and song data
+	    $('tr:regex(tabindex,-1)').each(function() {
+	        var artist = $(this).find('td:nth-child(3)').text() ? $(this).find('td:nth-child(3)').text() : songs[songs.length - 1][0]
+	        songs.push([
+	            artist,
+	            $(this).find('td:nth-child(4)').text()
+	            .replace(" Acoustic", '')
+	            .replace(" Chords", '')
+	            .replace(" Tab", '')
+	            .replace(" Ukulele", '')
+	            .replace(/\s\(ver \d+\)/, "")
+	        ]);
+	    });
+
+	    return songs;
+	}
+
+	function getTrack(accessToken, playlistID, callback) {
+	    var arrID = new Object();
+	    var indexSuc = 0;
+	    arrID.uris = [];
+	    var results = [];
+
+	    songs.forEach(function(song) {
+	        results.push($.ajax({
+	            type: "GET",
+	            url: "https://api.spotify.com/v1/search",
+	            data: {
+	                "q": song[0] + " " + song[1],
+	                "type": "track"
+	            },
+	            dataType: "json",
+	            contentType: "application/json",
+	            headers: {
+	                'Authorization': 'Bearer ' + accessToken,
+	            },
+	            success: function(data) {
+	                try {
+	                    arrID.uris.push("spotify:track:" + data.tracks.items[0].id)
+	                } catch (e) {
+	                    console.log(e);
+	                }
+	            }
+	        }));
+	        console.log(song[0]);
+	    })
+
+	    $.when.apply(this, results).done(function() {
+	        addToPlaylist(accessToken, idUser, playlistID, JSON.stringify(arrID.uris));
+	    });
+	}
--- content.js
+++ content.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+    alert("test");
+
     var iDiv = document.createElement('div');
-    iDiv.setAttribute("style", "position:absolute;height:100px;width:100px;top:45%;left:50%; border:0; margin-left:-50px;");
+    iDiv.setAttribute("style", "position:absolute;height:100px;width:100px;top:45%;left:10%; border:0; margin-left:-50px;");
     iDiv.innerHTML = "<button id='btn-login' href=''></button>";
     iDiv.id = 'block';
     iDiv.className = 'block';
@@ -9,6 +11,8 @@ $(document).ready(function() {
     document.getElementById('btn-login').setAttribute("style", "background:url(https://antoinemary.com/media/icon128.png) no-repeat;background-size:auto 100%; width:100%;height:100%;top:85px;border:0");
 
     var loginButton = document.getElementById('btn-login');
+
+
 
     loginButton.addEventListener('click', function() {
         login(function(accessToken) {
