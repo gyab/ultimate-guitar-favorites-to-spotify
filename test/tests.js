28b04d0e0b9ba02a420c6b2f3294947c885a25ed
anmary <contact@antoinemary.com>
2017-12-14 10:42:44 +0100

    first test up and running

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const Nexmo = require('nexmo');
 const ENV = require('./env');
 
 const SIGN_UP_SELECTOR = '.js-auth-sign-in-btn';
@@ -8,8 +9,15 @@ const SUBMIT_LOGIN_SELECTOR = '.ug-auth--btn__success';
 const TAB_SELECTOR = '.tr__lg.tr__actionable.js-favorite';
 const ARTIST_SELECTOR = TAB_SELECTOR + ' td:nth-child(1)';
 const SONG_SELECTOR = TAB_SELECTOR + ' td:nth-child(2)';
+const ARTIST_TEST = 'The Velvet Underground';
+const SONG_TEST = 'Sweet Jane';
 
 (async () => {
+    console.log('script start');
+    const nexmo = new Nexmo({
+        apiKey: ENV.NEXMO_API_KEY,
+        apiSecret: ENV.NEXMO_API_SECRET
+    })
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.ultimate-guitar.com/');
@@ -29,8 +37,13 @@ const SONG_SELECTOR = TAB_SELECTOR + ' td:nth-child(2)';
     const song = await page.evaluate((sel) => {
         return document.querySelector(sel).innerText.trim();
     }, SONG_SELECTOR);
-
-    console.log(artist);
-    console.log(song);
+    if (artist !== ARTIST_TEST || song !== SONG_TEST) {
+        const from = 'ug to spotify';
+        const to = env.number;
+        const text = 'it seems to be a problem with the extension';
+        nexmo.message.sendsms(from, to, text);
+        console.log('error, message sent');
+    }
+    console.log('script ended');
     await browser.close();
 })();
