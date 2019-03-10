9174d2090d7031a3551a718965c3c0a2a2d17a5a
Antoine Mary <_@antoinemary.com>
2019-03-10 09:12:14 +0100

    selector changes

--- app/spotify.js
+++ app/spotify.js
@@ -72,8 +72,8 @@ function getData(pSongsArr) {
     return new Promise(function(resolve, reject) {
 
         //For each song, get the artist and the song name
-        $("._1iQi2").each(function() {
-            if (!$(this).hasClass('_1Lagi')) {
+        $("._1sYjv").each(function() {
+            if (!$(this).hasClass('JR4du')) {
                 const allLinks = $(this).find('a');
                 const artist = allLinks.length === 2 ? allLinks[0].innerText.trim() : pSongsArr[pSongsArr.length - 1][0];
                 const song = allLinks.length === 2 ? allLinks[1].innerText : allLinks[0].innerText;
